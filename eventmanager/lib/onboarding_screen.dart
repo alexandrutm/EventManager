22ae1ad6783579b9cf9e29d4a93ec58ebe6fd8fd
AlexToma\Alexandru Toma <tomad.alexandru@gmail.com>
2022-11-22 01:14:27 +0200

    on boarding screen added

--- eventmanager/lib/onboarding_screen.dart
+++ eventmanager/lib/onboarding_screen.dart
@@ -1,4 +1,8 @@
+import 'package:eventmanager/intro_screen/intro_page1.dart';
+import 'package:eventmanager/intro_screen/intro_page2.dart';
+import 'package:eventmanager/intro_screen/intro_page3.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({Key? key}) : super(key: key);
@@ -8,8 +12,57 @@ class OnBoardingScreen extends StatefulWidget {
 }
 
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  //controller to keep track of which page we're on
+  final PageController _controller = PageController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            controller: _controller,
+            children: [
+              IntroPage1(),
+              IntroPage2(),
+              IntroPage3(),
+            ],
+          ),
+
+          //dot indicators
+          Container(
+            alignment: Alignment(0, 0.75),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //skip
+                GestureDetector(
+                  onTap: () {
+                    _controller.nextPage(
+                      duration: Duration(milliseconds: 500),
+                      curve: Curves.easeIn,
+                    );
+                  },
+                  child: Text('Skip'),
+                ),
+
+                SmoothPageIndicator(controller: _controller, count: 3),
+
+                //next or done
+                GestureDetector(
+                  onTap: () {
+                    _controller.nextPage(
+                      duration: Duration(milliseconds: 500),
+                      curve: Curves.easeIn,
+                    );
+                  },
+                  child: Text('Next'),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
