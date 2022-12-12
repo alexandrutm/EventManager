51ab4be463d29e3a35adc1fd047749fa4ac77111
AlexToma\Alexandru Toma <tomad.alexandru@gmail.com>
2022-12-12 21:19:22 +0200

    Update all versions

--- eventmanager/lib/main.dart
+++ eventmanager/lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:eventmanager/login_page.dart';
 import 'package:flutter/material.dart';
 import 'onboarding_screen.dart';
 import 'login_page.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const EventManager());
 }
 
