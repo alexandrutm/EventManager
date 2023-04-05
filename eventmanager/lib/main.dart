b14c8a3fecf09163621e53bf95b26b31255ab6f0
AlexToma\Alexandru Toma <tomad.alexandru@gmail.com>
2023-04-05 22:21:24 +0300

    dark mode

--- eventmanager/lib/main.dart
+++ eventmanager/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:eventmanager/pages/login_page.dart';
+import 'package:eventmanager/pages/settings/settings_page.dart';
 import 'package:eventmanager/providers/user_provider.dart';
 import 'package:eventmanager/responsive/mobile_screen_layout.dart';
 import 'package:eventmanager/responsive/responsive_layout.dart';
@@ -25,41 +26,46 @@ class EventManager extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => UserProvider(),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'EventManager',
-        theme: ThemeData.light(),
-        home: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.active) {
-              //Checking if the snapshot has any data or not
-              if (snapshot.hasData) {
-                // if snapshot has data which means user is logged in then we check the width of screen and accordingly display the screen layout
-                return const ResponsiveLayout(
-                  mobileScreenLayout: MobileScreenLayout(),
-                  webScreenLayout: WebScreenLayout(),
-                );
-              } else if (snapshot.hasError) {
-                return Center(
-                  child: Text('${snapshot.error}'),
+    final isDarkMode = Settings.getValue<bool>(SettingsPage.keyDarkMode);
+    return ValueChangeObserver<bool>(
+      cacheKey: SettingsPage.keyDarkMode,
+      defaultValue: false,
+      builder: (_, isDarkMode, __) => MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (_) => UserProvider(),
+          ),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'EventManager',
+          theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
+          home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active) {
+                //Checking if the snapshot has any data or not
+                if (snapshot.hasData) {
+                  // if snapshot has data which means user is logged in then we check the width of screen and accordingly display the screen layout
+                  return const ResponsiveLayout(
+                    mobileScreenLayout: MobileScreenLayout(),
+                    webScreenLayout: WebScreenLayout(),
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('${snapshot.error}'),
+                  );
+                }
+              }
+              // means connection to future hasnt been made yet
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
                 );
               }
-            }
-            // means connection to future hasnt been made yet
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            return const LoginScreen();
-          },
+              return const LoginScreen();
+            },
+          ),
         ),
       ),
     );
